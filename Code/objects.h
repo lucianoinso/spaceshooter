cd0dc3ea0698002f530141a8b51ec2a50ca1e714
lucianoinso <lucianoinso@gmail.com>
2018-09-19 02:41:51 -0300

    side shooter

---
+++ Code/objects.h
@@ -0,0 +1,76 @@
+//Object IDS
+enum IDS {PLAYER, BULLET, ENEMY};
+
+//Our Player
+struct SpaceShip
+{
+	int ID;
+	int x;
+	int y;
+	int lives;
+	int speed;
+	int boundx;
+	int boundy;
+	int score;
+
+	int maxFrame;
+	int curFrame;
+	int frameCount;
+	int frameDelay;
+	int frameWidth;
+	int frameHeight;
+	int animationColumns;
+	int animationDirection;
+
+	int animationRow;
+
+	ALLEGRO_BITMAP *image;
+};
+
+struct Bullet
+{
+	int ID;
+	int x;
+	int y;
+	bool live;
+	int speed;
+};
+
+struct Comet
+{
+	int ID;
+	int x;
+	int y;
+	bool live;
+	int speed;
+	int boundx;
+	int boundy;
+
+	int maxFrame;
+	int curFrame;
+	int frameCount;
+	int frameDelay;
+	int frameWidth;
+	int frameHeight;
+	int animationColumns;
+	int animationDirection;
+
+	ALLEGRO_BITMAP *image;
+};
+
+struct Explosion {
+	int x;
+	int y;
+	bool live;
+
+	int maxFrame;
+	int curFrame;
+	int frameCount;
+	int frameDelay;
+	int frameWidth;
+	int frameHeight;
+	int animationColumns;
+	int animationDirection;
+
+	ALLEGRO_BITMAP *image;
+};
